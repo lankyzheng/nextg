f60320dafd385a18eee8dbca3132fadbdeda1d2f
lankyzheng <lankyzheng@yahoo.com>
2013-01-20 20:09:54 +0800

    news type v0.1

--- wx_nextg.php
+++ wx_nextg.php
@@ -6,7 +6,10 @@
 //define your token
 define("TOKEN", "wannacool");
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+//$wechatObj->valid();
+$wechatObj->responseMsg();
+
+
 
 class wechatCallbackapiTest
 {
@@ -23,34 +26,57 @@ class wechatCallbackapiTest
 
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	//get post data, May be due to the different environments
+	$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
       	//extract post data
-		if (!empty($postStr)){
+	if (!empty($postStr)){
                 
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+		$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        	$fromUsername = $postObj->FromUserName;
+        	$toUsername = $postObj->ToUserName;
+        	$keyword = trim($postObj->Content);
+        	$time = time();
+        /*	$textTpl = "<xml>
+			<ToUserName><![CDATA[%s]]></ToUserName>
+			<FromUserName><![CDATA[%s]]></FromUserName>
+			<CreateTime>%s</CreateTime>
+			<MsgType><![CDATA[%s]]></MsgType>
+			<Content><![CDATA[%s]]></Content>
+			<FuncFlag>0</FuncFlag>
+			</xml>";
+	*/
+		$newsTpl = "<xml>
+			<ToUserName><![CDATA[%s]]></ToUserName>
+			<FromUserName><![CDATA[%s]]></FromUserName>
+			<CreateTime>%s</CreateTime>
+			<MsgType><![CDATA[%s]]></MsgType>
+			<Content><![CDATA[]]></Content>
+			<ArticleCount>1</ArticleCount>
+			<Articles>
+				<item>
+				<Title><![CDATA[%s]]></Title>
+				<Description><![CDATA[%s]]></Description>
+				<PicUrl><![CDATA[%s]]></PicUrl>
+				<Url><![CDATA[%s]]></Url>
+				</item>
+			</Articles>
+			<FuncFlag>1</FuncFlag>
+			</xml>";
+
+			if(!empty( $keyword ))
+	                {
+        	      		$msgType = "text";
+                		// $contentStr = "Welcome to wechat world!";
+				$picTitle = "N for next";
+				$picDesc = "just reply N to see next";
+				$picUrl = "http://v2.wannacool.com/ntest.jpg";
+				$picClickUrl = "http://v2.wannacool.com/ntest.jpg";
+                		$resultStr = sprintf($newsTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr, $picTitle, $picDesc, $picUrl, $picClickUrl);
+                		echo $resultStr;
+                	}else{
+                		echo "Input something...";
+                	}	
 
         }else {
         	echo "";
